1b19367dd8b23f234f5e3e15626f73916f294f48
kumar <kumarajay912282@gmail.com>
2026-08-10 10:55:11 +0530

    Add implementation of recursive function to calculate the sum of n digits

---
+++ R5.java
@@ -0,0 +1,14 @@
+public class R5 {
+    // sum of n digit where n= 5
+    public static void main(String[] args){
+      int ans = sum(5);
+      System.out.print(ans);
+    }
+    static int sum(int n){
+        if (n==1){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+    
+}
