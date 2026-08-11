2ac083899f004bbbfe628cc67bf483351eed90dc
kumar <kumarajay912282@gmail.com>
2026-08-11 11:57:33 +0530

    Add implementation of recursive function to find a target in an array

---
+++ R13.java
@@ -0,0 +1,13 @@
+public class R13 {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 5, 18, 9};
+        System.out.println(find(arr,18,0));
+    }
+    static boolean find(int[] arr, int target, int index){
+        if(index == arr.length){
+            return false;
+        }
+        return arr[index] == target || find(arr,target, index+1);
+    }
+    
+}
