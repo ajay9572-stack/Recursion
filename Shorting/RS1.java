ad232a87137fe69a2bdada4743b1dd6fa4d22f53
kumar <kumarajay912282@gmail.com>
2026-08-12 21:08:04 +0530

    Add implementation of bubble sort using recursion

---
+++ Shorting/RS1.java
@@ -0,0 +1,34 @@
+package Shorting;
+
+import java.util.Arrays;
+// bubble shorting
+
+public class RS1 {
+   public static void main(String[] args) {
+
+    int []arr = {4, 3, 2, 1};
+    bubble(arr, arr.length-1, 0);
+    System.out.println(Arrays.toString(arr));    // for print the shorted array
+
+   } 
+
+   static void bubble(int []arr, int r, int c){
+    if(r==0){
+        return;
+    }
+    if(c<r){
+
+        if(arr[c]>arr[c+1]){
+            //swap
+            int temp = arr[c];
+            arr[c] = arr[c+1];
+            arr[c+1] = temp;
+        }
+
+        bubble(arr, r, c+1);
+    } 
+    else{
+        bubble(arr, r-1, 0);
+    }
+   }
+}
